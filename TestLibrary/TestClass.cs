9feca5601fe865388c2f915d87f0ea1375db141d
Har Amrit Pal Singh Panesar <panehaps@myumanitoba.ca>
2018-11-07 15:44:41 -0800

    Add project files.

---
+++ TestLibrary/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        public static string GetValue(int input)
+        {
+            return input.ToString() ;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLibrary;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int input = 1;
+
+
+            //Act
+            string output = TestClass.GetValue(input);
+
+
+            //Assert
+            Assert.AreEqual("sdf", output);
+        }
+    }
+}
